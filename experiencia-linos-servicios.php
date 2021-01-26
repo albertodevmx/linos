bec1341d028bc4ebca295c95dabaa51aa4091b6b
Alberto Hernandez <albertomxio@mac.local>
2021-01-26 02:49:21 -0600

    Antes de dormir

--- experiencia-linos-servicios.php
+++ experiencia-linos-servicios.php
@@ -1,70 +1,31 @@
 <?php include("header.php"); ?>
 
 
+<div class="container-fluid d-flex align-items-center portada" style="--background: url('./img/banners/banquetes-banner-1-principal.jpg'); --opacity: 1;">
+    <div class="portada__info pt-5">
+        <h1 class="mt-5 fondo-negro-transparente d-inline-block p-1 pl-2 pr-2">Baquetes Japonese Gournet</h1>
+        <div></div>
+        <p class="fondo-negro-transparente d-inline-block p-1">
+            Disfruta de un delicioso y original menú de comida japonesa de lujo para tus eventos sociales como bodas, <br>quince años, bautizos, aniversarios e incluso para tus eventos empresariales y sociales.
+        </p>
+    </div>
+</div>
+
 
 
 
 <div class="container-fluid content push">
     <div class="container">
     
-                <div class="row">
-                    <div class="col-12 p-3 ">
-                        <h3>
-                            Experiencia Linos - Servicios
-                        </h3>
-                    </div>
-                </div>
+        <div class="row">
+            <div class="col-12 col-md-6 p-3 p-md-5">
+                <h3>Mesas Tepanyaki</h3>
+                <p>
+                    Servicio especial para servir tepanyaki al momento con mobiliario especial. ¡Haz de tu evento algo espectacular!
+                </p>
+            </div>
+        </div>
 
-                <div class="row">
-                    <div class="col-12 col-md-6 p-5 text-light">
-
-
-
-                    <h3>BUFFET</h3>
-                    <p>Este es un servicio adecuado para todo tipo de menús complementos y bocadillos. Lino´s incluye todos los artículos de decoración de buffet y nuestro personal se encarga de atender personalmente a sus invitados los platillos son cocinados al momento (dependiendo del menú que elija).</p>
-
-                    <hr>
-                    <h3>MESAS </h3>
-                    <p>Adecuado para ocasiones formales u otro tipo de reunión para servir menús completos con opciones de platillos como Teppan  Yaki. Cada invitado cuenta con su servicio individual y sus platillos son presentados en artículos especiales japoneses.  </p>
-
-                    <hr>
-                    <h3>CHAROLEO </h3>
-                    <p>En este servicio puede contarse con un buffet o únicamente al charoleo de todos los bocadillos. Es adecuado cuando no se cuenta con espacio suficiente para tener el mobiliario como mesas o salas. La charola es continuamente resurtida y redistribuida por toda el área donde se encuentran sus invitados. Recomendado para menú sendai.  </p>
-
-                    <hr>
-                    <h3>PREPARACIÓN </h3>
-                    <p>Contamos con el mobiliario necesario para preparar todo al momento, pero es requerido un espacio techado iluminado y con una toma de agua cercana. Esto es para garantizar la óptima calidad en la preparación de los platillos.  </p>
-
-                    <hr>
-                    <h3>CÓCTEL </h3>
-                    <p>Es un servicio adecuado para todo tipo de evento social o de negocios. Puede ser presentado o servido de diferentes formas: charoleo, buffet, mesas, bar, etc. De igual forma es cocinado al momento en menú sendai.</p>
-
-
-
-
-                    <hr>
-                    {galería de Servicios 12 a 20 fotos}
-                    {Poner galería de Servicios 12 a 20 fotos}
-
-
-                    Con gusto te atenderemos
-                    Contáctanos y vive una experiencia gourmet inigualable.
-                    {col 1}
-                    Nombre
-                    Teléfono
-
-                    O si lo prefieres haz contacto por
-                    Tel. 5822 3521 y 5824 94437
-                    WhatsApp. 55 4084 3940
-                    {col 2}
-                    {imagen}
-                    {poner qr de contacto}
-
-
-                        
-
-                    </div>
-                </div>
     </div>
 
 
