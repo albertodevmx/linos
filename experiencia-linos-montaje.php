16ebd512789710eee342238019da32e1b54afe0d
Alberto Hernandez <albertomxio@mac.local>
2021-01-28 01:19:21 -0600

    Se añadió montaje

--- experiencia-linos-montaje.php
+++ experiencia-linos-montaje.php
@@ -7,142 +7,155 @@
 <div class="container-fluid content push">
     <div class="container">
     
-                <div class="row">
-                    <div class="col-12 p-3 ">
-                        <h3>
-                            Experiencia Linos - Montaje
-                        </h3>
-                    </div>
-                </div>
+        <div class="row">
+            <div class="col-12 p-3 ">
+                <h3>
+                    Experiencia Linos - Montaje
+                </h3>
+            </div>
+        </div>
                 
 
 
 
-        <div class="row mb-5">
-            <div class="col-12 col-md-6 p-3 p-md-3">
-                <div class="card bg-mi-rojo">
-                    <div class="card-body menu-bloque-rojo">
-                        <div class="row">
-                            <div class="col-12">
-                                <h4 class="text-center p-3">ARTÍCULOS PARA MONTAJE DE PLATILLOS</h4>
-                            </div>
-
-                            <div class="col-12 text-center">
-                                <img src="./img/galeria/eventos-galerias-grandes/eventos-galeria-05-grande.jpg" alt="" class="w-100 p-3">
-                            </div>
-
-                            <div class="col-12 p-3">
-                                <p>
-                                Artículos especiales para montar los platillos de losa japonesa, así como para servir la comida ya sea tipo buffet o servicios a mesas.
-                                </p>
-                                
-                                <h4 class="mt-5">ARTÍCULOS DE CRISTALERÍA</h4>
-                                <p>Renta de cristalería por piezas o en paquete todo incluido.</p>
-                                
-                                <h4 class="mt-5">SERVICIO DE MESEROS PARA BAR </h4>
-                                <p>Contratados para servicio de bebida con opción a tiempo extra.</p>
-                                
-                                <h4 class="mt-5">NITRO SHOTS </h4>
-                                <p>Preparados al momento con nitrógeno y perlas moleculares. Mezclados con distintos licores: sake, vodka, tequila, ron, midori. </p>
-                                
-                                <h4 class="mt-5">BARRA DE SUSHI y SUSHI BOWL </h4>
-                                <p>Barras de Sushi Sushi Bowl preparados al momento.</p>
-                                
-                                <h4 class="mt-5">SERVICIO DE CAFÉ Y TÉ</h4>
-                                
-                                <h4 class="mt-5">SERVICIO DE SAKE FRÍO</h4>
-
-                                <h4 class="mt-5">ARTÍCULOS DE CRISTALERÍA EN PAQUETE </h4>
-                                
-                                <h4 class="mt-5">ARTÍCULOS DE CRISTALERÍA </h4>
-                                <img src="./img/galeria/eventos-galerias-grandes/eventos-galeria-10-grande.jpg" alt="" class="w-100 p-3">
-                                <p>
-                                Vaso high ball, vaso old fashion 
-                                Copa para vino, tequilero
-                                Cenicero 
-                                Charola para bar
-                                hielera con pinzas 
-                                Tabla, cuchillo, exprimidor
-                                Sal, limones 
-                                Maggi, inglesa, salsa Tabasco 
-                                Vaso desechable 
-                                Servilletas 
-                                Bolsa para basura 
-                                </p>
-
-                                <h4 class="mt-5">SERVICIO DE CAFÉ INCLUYE: (CON COSTO ADICIONAL) </h4>
-                                <p>Cafetera, coffemate, té surtidos, café americano, café descafeinado, kit de cuchara y azúcar, servido en taza negra alargada. Mínimo 15 piezas.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="row">
+            
+            <div class="col-12 col-md-5 text-center">
+                <img src="./img/galerias/montaje-y-cristaleria/montaje-08.jpg" alt="" class="w-100 p-3">
             </div>
 
-            <div class="col-12 col-md-6 p-3 p-md-3">
-                <div class="card bg-mi-rojo">
-                    <div class="card-body menu-bloque-rojo">
-                        <div class="row">
+            <div class="col-12 col-md-7 p-3">
+                <h4 class="pb-3">ARTÍCULOS PARA MONTAJE DE PLATILLOS</h4>
+                <p>
+                Artículos especiales para montar los platillos de losa japonesa, así como para servir la comida ya sea tipo buffet o servicios a mesas.
+                </p>
+                
+                <h4 class="mt-5">ARTÍCULOS DE CRISTALERÍA</h4>
+                <p>Renta de cristalería por piezas o en paquete todo incluido.</p>
+                
+                <h4 class="mt-5">SERVICIO DE MESEROS PARA BAR </h4>
+                <p>Contratados para servicio de bebida con opción a tiempo extra.</p>
+                
+                <h4 class="mt-5">NITRO SHOTS </h4>
+                <p>Preparados al momento con nitrógeno y perlas moleculares. Mezclados con distintos licores: sake, vodka, tequila, ron, midori. </p>
+                
+                <h4 class="mt-5">BARRA DE SUSHI y SUSHI BOWL </h4>
+                <p>Barras de Sushi Sushi Bowl preparados al momento.</p>
+                
+                <h4 class="mt-5">SERVICIO DE CAFÉ Y TÉ</h4>
+                
+                <h4 class="mt-5">SERVICIO DE SAKE FRÍO</h4>
 
-                            <div class="col-12">
-                                <h4 class="text-center text-md-left p-3 pl-md-0">POLÍTICAS DE RESERVACIÓN PARA CRISTALERÍA</h4>
-                            </div>
+                <h4 class="mt-5">ARTÍCULOS DE CRISTALERÍA EN PAQUETE </h4>
+                
+                <h4 class="mt-5">ARTÍCULOS DE CRISTALERÍA </h4>
+                <img src="./img/galeria/eventos-galerias-grandes/eventos-galeria-10-grande.jpg" alt="" class="w-100 p-3">
+                <p>
+                Vaso high ball, vaso old fashion 
+                Copa para vino, tequilero
+                Cenicero 
+                Charola para bar
+                hielera con pinzas 
+                Tabla, cuchillo, exprimidor
+                Sal, limones 
+                Maggi, inglesa, salsa Tabasco 
+                Vaso desechable 
+                Servilletas 
+                Bolsa para basura 
+                </p>
 
-                            <div class="col-12 text-center">
-                                <img src="./img/galeria/eventos-galerias-grandes/eventos-galeria-06-grande.jpg" alt="" class="w-100 p-3">
-                            </div>
-
-                            <div class="col-12 p-3 pr-md-5">
-                                
-                            
-                                <p class="text-center text-md-left">
-                                    <ul>
-                                        <li>La venta en paquete de cristalería es por un mínimo de 20 paquetes los cuales contienen los artículos de cristalería necesarios para el servicio.</li>
-                                        <li>La cristalería se recoge al retirarse el último mesero del lugar del servicio y dejar a las piezas suficientes de vasos desechables de cortesía para el cliente.</li>
-                                        <li>La cristalería sólo se realizará en conjunto con nuestra brigada de meseros ya que nuestra política no contempla cargos adicionales por pérdidas o rupturas.</li>
-                                        <li>No incluye mezcladores (refrescos, hielo, alcohol).</li>
-                                        <li>No incluye servicio de meseros para bar.    </li>
-                                    </ul>
-                                    
-
-                                    <h4 class="mt-5">DETALLES DE SERVICIOS, EQUIPOS DE RENTA Y MENUS</h4>
-                                    <ul>
-                                        <li>Vajilla de comida tradicional, plato trinche redondo o cuadrado</li>
-                                        <li>Salseras, arroceras, tenedor, cuchara de postre, palillos.</li>
-                                        <li>Arreglos para su decoración de la mesa buffet, </li>
-                                        <li>Equipo de preparación (módulo con parrilla, freidora y plancha),</li>
-                                        <li>Artículos especiales japoneses para montaje de comida,</li>
-                                        <li>Cocineros y meseros exclusivos para comida.</li>
-                                    </ul>
-                                    
-
-                                    <h4 class="mt-5">ESTE EQUIPO NO LO INCLUYE:</h4>
-                                    <ul>
-                                        <li>Mesas, sillas, manteles y centros de mesa,</li>
-                                        <li>Servilletas de tela (opcionales),</li>
-                                        <li>Tablones para bar y para buffet,</li>
-                                        <li>Bebida y hielos, servicio con costo adicional</li>
-                                        <li>Cristalería. servicio con costo adicional</li>
-                                    </ul>
-
-                                    <h4 class="mt-5">NUESTROS MENÚS INCLUYEN: Montaje y cristalería</h4>
-                                    <ul>
-                                        <li>Plato trinche, redondo o cuadrado,</li>
-                                        <li>Salseras, arroceras,</li>
-                                        <li>Palillos, cubierto y artículos especiales,</li>
-                                        <li>Cocineros y meseros para servir la comida.</li>
-                                    </ul>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <h4 class="mt-5">SERVICIO DE CAFÉ INCLUYE: (CON COSTO ADICIONAL) </h4>
+                <p>Cafetera, coffemate, té surtidos, café americano, café descafeinado, kit de cuchara y azúcar, servido en taza negra alargada. Mínimo 15 piezas.</p>
             </div>
         </div>
 
 
+
+
+        <div class="row mt-5 mb-5 galeria">
+
+                <!-- Montaje -->
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-01.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-01.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-02.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-02.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-03.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-03.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-04.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-04.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-05.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-05.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-06.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-06.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-07.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-07.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+                <div class="col-6 col-md-2 mb-5"><a href="./img/galerias/montaje-y-cristaleria/montaje-08.jpg" data-fancybox="gallery" data-caption="Montaje y cristalería"><img src="./img/galerias/montaje-y-cristaleria/th/montaje-08.jpg" class="w-100 img-thumbnail shadow-sm"></a></div>
+
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-12">
+                <h3 class="p-3">
+                    POLÍTICAS DE RESERVACIÓN PARA CRISTALERÍA
+                </h3>
+            </div>
+        </div>
+
+
+
+        <div class="row mb-5">
+            <div class="col-12 col-md-6 p-3 pr-md-5">
+
+                <p class="text-center text-md-left">
+                    <ul>
+                        <li>La venta en paquete de cristalería es por un mínimo de 20 paquetes los cuales contienen los artículos de cristalería necesarios para el servicio.</li>
+                        <li>La cristalería se recoge al retirarse el último mesero del lugar del servicio y dejar a las piezas suficientes de vasos desechables de cortesía para el cliente.</li>
+                        <li>La cristalería sólo se realizará en conjunto con nuestra brigada de meseros ya que nuestra política no contempla cargos adicionales por pérdidas o rupturas.</li>
+                        <li>No incluye mezcladores (refrescos, hielo, alcohol).</li>
+                        <li>No incluye servicio de meseros para bar.    </li>
+                    </ul>
+                    
+
+                    <h4 class="mt-5">DETALLES DE SERVICIOS, EQUIPOS DE RENTA Y MENUS</h4>
+                    <ul>
+                        <li>Vajilla de comida tradicional, plato trinche redondo o cuadrado</li>
+                        <li>Salseras, arroceras, tenedor, cuchara de postre, palillos.</li>
+                        <li>Arreglos para su decoración de la mesa buffet, </li>
+                        <li>Equipo de preparación (módulo con parrilla, freidora y plancha),</li>
+                        <li>Artículos especiales japoneses para montaje de comida,</li>
+                        <li>Cocineros y meseros exclusivos para comida.</li>
+                    </ul>
+
+                </p>
+            </div>
+
+
+            <div class="col-12 col-md-6 p-3 pr-md-5">
+
+                <p class="text-center text-md-left">
+
+                    <h4 class="mt-4">ESTE EQUIPO NO LO INCLUYE:</h4>
+                    <ul>
+                        <li>Mesas, sillas, manteles y centros de mesa,</li>
+                        <li>Servilletas de tela (opcionales),</li>
+                        <li>Tablones para bar y para buffet,</li>
+                        <li>Bebida y hielos, servicio con costo adicional</li>
+                        <li>Cristalería. servicio con costo adicional</li>
+                    </ul>
+
+                    <h4 class="mt-5">NUESTROS MENÚS INCLUYEN: Montaje y cristalería</h4>
+                    <ul>
+                        <li>Plato trinche, redondo o cuadrado,</li>
+                        <li>Salseras, arroceras,</li>
+                        <li>Palillos, cubierto y artículos especiales,</li>
+                        <li>Cocineros y meseros para servir la comida.</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+
+
+
+
+        
+
+
     </div>
-
-
 
 </div>
 
